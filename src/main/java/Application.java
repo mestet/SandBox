6ad40d292f8d726ec9a8cc44ca8b079f59fd569e
MVlasov <mvlasov@luxoft.com>
2021-09-10 22:07:57 +0300

    Old stuff push

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,33 +1,36 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
+import utils.Hero;
 
 
 @Configuration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+
+
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate rt = new RestTemplate();
-
-        // Mapping objects to JSON
-        rt.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-
-        // Custom error handling
-        rt.setErrorHandler(new DefaultResponseErrorHandler() {
-            protected boolean hasError(HttpStatus statusCode) {
-                return false;
-            }
-        });
-
-        return rt;
+    private static void printHero() {
+        Hero hero = new Hero("Gilraen", "10C9E400");
+        System.out.println("Hero " + hero.getName() + " attributes: ");
+        System.out.println(hero.toString());
     }
+
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        RestTemplate rt = new RestTemplate();
+//
+//        // Mapping objects to JSON
+//        rt.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+//
+//        // Custom error handling
+//        rt.setErrorHandler(new DefaultResponseErrorHandler() {
+//            protected boolean hasError(HttpStatus statusCode) {
+//                return false;
+//            }
+//        });
+//
+//        return rt;
+//    }
 }
